bb3b903d4a1406bb368f22e3618616cd6e12d8b5
basava1139 <106962015+basava1139@users.noreply.github.com>
2024-08-13 11:02:46 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "cicd-terraform-eks"
-    key    = "jenkins/terraform.tfstate"
-    region = "us-east-1"
+    bucket = "jenkinsbucket113"
+    key    = "jenkinsbucket113/terraform.tfstate"
+    region = "ap-south-1"
   }
 }
